4c5a619e507b83a9a5cd9110e231fe2e897f0c20
ekanshu-dev <ekanshu.dev@gmail.com>
2022-04-12 02:49:59 +0530

    basic to-go

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import "./App.css";
 import { useState } from "react";
 import Navbar from "./components/Navbar";
 import Form from "./components/Form";
-import Alert from "./components/Alert";
 import About from "./components/About";
 import Error from "./components/Error";
 import {
@@ -14,28 +13,14 @@ import {
 
 function App() {
   const [mode, setMode] = useState("light");
-  const [alert, setAlert] = useState(null);
-
-  const showAlert = (message, type) => {
-    setAlert({
-      type: type,
-      message: message,
-    });
-
-    setTimeout(() => {
-      setAlert(null);
-    }, 1500);
-  };
 
   const toggleMode = () => {
     if (mode === "light") {
       setMode("dark");
       document.body.style.backgroundColor = "black";
-      showAlert("Dark Mode Enabled", "Success!");
     } else {
       setMode("light");
       document.body.style.backgroundColor = "white";
-      showAlert("Dark Mode Disabled", "Success!");
     }
   };
 
@@ -48,7 +33,6 @@ function App() {
           mode={mode}
           toggleMode={toggleMode}
         />
-        <Alert alert={alert} />
         <Routes>
           <Route exact path="/" element={<Form heading="Text-Area" mode={mode} />} />
           <Route exact path="/about" element={<About mode={mode} />} />
